3d8fa9b5490b3f07d80f90ac33040caf338268eb
Josh Guo <joshua.guo@stonybrook.edu>
2021-01-24 02:35:56 -0500

    Added tech stack section to projects and begun work on contact section

--- src/components/App.js
+++ src/components/App.js
@@ -30,9 +30,17 @@ function App() {
             paddingLeft: isMobile() ? '5%' : '20%',
             paddingTop: window.screen.height/4 + "px",
             paddingRight: isMobile() ? '5%' : '20%',
-            paddingBottom: window.screen.height/4 + "px",
+            paddingBottom: window.screen.height/5 + "px",
             backgroundColor: color2,
             color: color1
+        },
+        contactWrapper: {
+            paddingLeft: isMobile() ? '5%' : '20%',
+            paddingTop: window.screen.height/4 + "px",
+            paddingRight: isMobile() ? '5%' : '20%',
+            paddingBottom: window.screen.height/4 + "px",
+            backgroundColor:  color1,
+            color: color2,
         }
     } 
 
@@ -46,8 +54,12 @@ function App() {
 
             <div style={styles.projectWrapper}>
                 <Header name={json.data.header2} backgroundColor={color2} fontColor={color1} lineColor={color1}/>
-                <ScrollAnimation style={styles.arrow} animateIn='fadeIn' delay={2000} animateOnce={true}>&#8595;</ScrollAnimation>
+                <div style={{marginBottom: window.screen.height/8 + "px"}} />
                 <ProjectList projects={json.data.projects} color1={color1} color2={color2}/>
+            </div>
+
+            <div style={styles.contactWrapper}>
+                <Header name={json.data.header3} backgroundColor={color1} fontColor={color2} lineColor={color2}/>
             </div>
             
         </div>
